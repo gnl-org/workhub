29925fe6c7b2f9942e6e5899dba01a70e7d958ba
Gajendra N L <gajendra.wbb@gmail.com>
2026-07-12 17:02:34 +0530

    disable cache in prod for now

--- backend/core-service/src/main/java/com/gnl/workhub/coreservice/config/CacheConfig.java
+++ backend/core-service/src/main/java/com/gnl/workhub/coreservice/config/CacheConfig.java
@@ -1,5 +1,6 @@
 package com.gnl.workhub.coreservice.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
@@ -16,6 +17,7 @@ import java.util.List;
 import java.util.Map;
 
 @Configuration
+@ConditionalOnProperty(name = "spring.cache.type", havingValue = "redis")
 public class CacheConfig {
 
     @Bean
